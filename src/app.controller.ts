491e28f429933921cae7808c00784e52375ab2ef
Saragam Ghimire <codewithghimire@gmail.com>
2023-02-08 20:09:22 +0545

    use rate limit globally, skip limiter for certain controller and different limitter for different controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 import { AppService } from './app.service';
 
+/* @SkipThrottle() //skip throttle for whole controller */
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @SkipThrottle()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+
+  @Get('users')
+  getUsers(){
+    return ['ram','shyam','hari']
+  }
+
+  @Throttle(5,20)
+  @Get('products')
+  getProducts(){
+    return ['mobile','laptop','headphone']
   }
 }
